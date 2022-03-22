611370dd1850483c4502e70f98a55fd27a37016c
DessertHeart <daizel@163.com>
2022-03-22 11:34:14 +0800

    update

--- contracts/MyTokenMarket.sol
+++ contracts/MyTokenMarket.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "./IUniswapV2Router01.sol";
 import "./@openzeppelin/contracts/token/IERC20";
+import "./IMiniChefV2.sol";
+import "./IRewarder.sol";
 
 contract MyTokenMarket is  {
 
@@ -12,16 +14,29 @@ contract MyTokenMarket is  {
     
     // Router合约地址
     address router;
+
+    // MasterChef合约地址
+    address miniChef
+
+    // index of pool
+    uint256 pid = IERC20(myToken).length -1;
     
-    constructor(address _myToken, address _weth, address _router) {
+    constructor(
+        address _myToken, 
+        address _weth, 
+        address _router,
+        address _miniChef
+        ) 
+    {
         myToken = _myToken;
         weth = _weth;
         router = _router;
+        miniChef = _miniChef;
     }
 
     // 添加MyToken与ETH流动池
     function addLiquidity(uint _myTokenNum, address _to, uint256 _amount) payable public {
-       IERC20(myToken).transferFrom(msg.sender, router, _amount);
+       IERC20(myToken).transferFrom(msg.sender, address(this), _amount);
        IERC20(myToken).approve(router, _amount);
        
        IUniswapV2Router01(router).addLiquidityETH{value:msg.value}(
@@ -48,13 +63,23 @@ contract MyTokenMarket is  {
         )
     }
 
-    // // 质押
-    // function deposit() public {
+    // 质押：应该是输入LP代币的地址？
+    function deposit(uint256 _lpamount, address _rewarder) public {
 
-    // }
+        // 添加pool
+        IMiniChefV2(miniChef).add(1, IERC20(myToken), IRewarder(_rewarder));
 
-    // // 提款
-    // function withdraw() public {
+        IERC20(myToken).transferFrom(msg.sender, address(this), _lpamount);
+        IERC20(myToken).approve(miniChef, _lpamount);
+        
+        IMiniChefV2(miniChef).deposit(pid, _lpamount, msg.sender);
+    }
 
-    // }
+    // 提款
+    function withdraw(uint256 _lpamount) public {
+        IERC20(myToken).transferFrom(msg.sender, address(this), _amount);
+        IERC20(myToken).approve(miniChef, _amount);
+
+        IMiniChefV2(miniChef).withdraw(pid, _lpamount, msg.sender)
+    }
 }
