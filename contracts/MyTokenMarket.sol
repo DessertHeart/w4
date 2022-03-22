a8d7924a2a3c9bc42898fa4f105bca298a428d14
DessertHeart <daizel@163.com>
2022-03-22 21:58:25 +0800

    update

--- contracts/MyTokenMarket.sol
+++ contracts/MyTokenMarket.sol
@@ -2,55 +2,62 @@
 pragma solidity ^0.8.0;
 
 import "./IUniswapV2Router01.sol";
-import "./@openzeppelin/contracts/token/IERC20";
-import "./IMiniChefV2.sol";
-import "./IRewarder.sol";
+import "./@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract MyTokenMarket is  {
+interface IMasterChef {
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function withdraw(uint256 _pid, uint256 _amount) external;
+}
+
+contract MyTokenMarket {
 
     // 交易对代币
-    address myToken;
-    address weth;
+    address private immutable myToken;
+    address private immutable weth;
     
     // Router合约地址
-    address router;
+    address private immutable router;
 
     // MasterChef合约地址
-    address miniChef
+    address private immutable masterChef;
 
     // index of pool
-    uint256 pid = IERC20(myToken).length -1;
+    uint256 private immutable pid;
+
+    // 余额查询
+    mapping(address => uint256) userBalance;
     
     constructor(
         address _myToken, 
         address _weth, 
         address _router,
-        address _miniChef
+        address _masterChef,
+        uint256 _pid
         ) 
     {
         myToken = _myToken;
         weth = _weth;
         router = _router;
-        miniChef = _miniChef;
+        masterChef = _masterChef;
+        pid = _pid;
     }
 
     // 添加MyToken与ETH流动池
-    function addLiquidity(uint _myTokenNum, address _to, uint256 _amount) payable public {
-       IERC20(myToken).transferFrom(msg.sender, address(this), _amount);
-       IERC20(myToken).approve(router, _amount);
+    function addLiquidity(uint _myTokenNum, address _to) payable public returns(uint256 amountToken, uint256 amountETH, uint256 liquidity) {
+        IERC20(myToken).transferFrom(msg.sender, address(this), _myTokenNum);
+        IERC20(myToken).approve(router, _myTokenNum);
        
-       IUniswapV2Router01(router).addLiquidityETH{value:msg.value}(
-        mytoken,
-        _myTokenNum,
-         0,
-         0,
-        _to,
-         block.timestamp
-       );
+        (amountToken, amountETH, liquidity) = IUniswapV2Router01(router).addLiquidityETH{value:msg.value}(myToken, _myTokenNum, 0, 0, _to, block.timestamp);
+        // 退回
+        if(amountToken < _myTokenNum) {
+            IERC20(myToken).transferFrom(address(this), msg.sender, _myTokenNum - amountToken);
+        }else if(amountETH < msg.value){
+            address(this).call{value: msg.value - amountETH}("");
+        }
     }
     
     // 购买MyToken
-    function buyToken(uint256 _amount) payable public {
+    function buyToken(address _to) payable public {
         address[] memory path = new address[](2);
         path[0] = weth;
         path[1] = myToken;
@@ -58,28 +65,40 @@ contract MyTokenMarket is  {
         IUniswapV2Router01(router).swapExactETHForTokens{value:msg.value}(
             0, 
             path, 
-            msg.sender,
+            _to,
             block.timestamp
-        )
+        );  
     }
 
-    // 质押：应该是输入LP代币的地址？
-    function deposit(uint256 _lpamount, address _rewarder) public {
+    // 质押
+    function deposit(address _to) payable public returns(uint256[] memory amount) {
+        address[] memory path = new address[](2);
+        path[0] = weth;
+        path[1] = myToken;
+        amount = IUniswapV2Router01(router).swapExactETHForTokens{value:msg.value}(
+            0, 
+            path, 
+            _to,
+            block.timestamp
+        ); 
+        // 多退
+        if(amount[0] < msg.value){
+            address(this).call{value: msg.value - amount[0]}("");
+        }
+        IERC20(myToken).approve(masterChef, amount[1]);
+        IMasterChef(masterChef).deposit(pid, amount[1]);
+        userBalance[_to] = amount[1];
 
-        // 添加pool
-        IMiniChefV2(miniChef).add(1, IERC20(myToken), IRewarder(_rewarder));
-
-        IERC20(myToken).transferFrom(msg.sender, address(this), _lpamount);
-        IERC20(myToken).approve(miniChef, _lpamount);
-        
-        IMiniChefV2(miniChef).deposit(pid, _lpamount, msg.sender);
+        return amount;
     }
 
     // 提款
-    function withdraw(uint256 _lpamount) public {
-        IERC20(myToken).transferFrom(msg.sender, address(this), _amount);
-        IERC20(myToken).approve(miniChef, _amount);
+    function withdraw(address _to) public returns(bool){
+        require(msg.sender == _to, "Wrong guys");
+        uint256 amount = userBalance[_to];
+        userBalance[_to] = 0;
+        IMasterChef(masterChef).withdraw(pid, amount);
 
-        IMiniChefV2(miniChef).withdraw(pid, _lpamount, msg.sender)
+        return true;
     }
 }
